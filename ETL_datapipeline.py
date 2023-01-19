f7b7200c6fcebeb4591d957b7294b39188d07a68
Fabiola Marchì <mrcfbl99s48c342j@studium.unict.it>
2023-01-19 18:51:45 +0100

    k

--- ETL_datapipeline.py
+++ ETL_datapipeline.py
@@ -10,12 +10,12 @@ from prometheus_api_client.utils import parse_datetime
 prom = PrometheusConnect(url="http://15.160.61.227:29090", disable_ssl=True)
 
 #Query on Prom
+#calcoli un set di metadati con i relativi valori (autocorrelazione? stazionarietà? stagionalità?)
 
 start_time = parse_datetime("1h")
 end_time = parse_datetime("now")
 chunk_size = timedelta(minutes=5)
 label_config = {'job': 'summary'} 
-#solo il job,cosi mi fa la media di tutte le istanze dei job
 metrics_to_evaluate = ['cpuLoad', 'availableMem', 'diskUsage']
 for metric in metrics_to_evaluate:
     metric_data = prom.get_metric_range_data(
@@ -30,21 +30,84 @@ for metric in metrics_to_evaluate:
                                                   #metrics downloaded using get_metric query
      #    my_metric_object = metric_object_list[0] # one of the metrics from the list
     #print(my_metric_object)
+
     fields = ['Date', 'Time', 'CPULoad'] 
-
-    
-
+  
     for item in metric_object_list:
         print(item)
 
+    metric_object_list.to_csv('./export_dataframe.csv')
 
-for item in metric_object_list:
-    with open('export_dataframe.csv', 'a') as f:
-            csv_writer = csv.writer(f)
-            csv_writer.writerow(fields)
-            csv_writer.writerow(item)
 
-print("#-------------------#")
+#- calcoli il valore di max, min, avg, dev_std della metriche per 1h,3h, 12h;
+
+metric_df = MetricRangeDataFrame(metric_data)
+print(metric_df.head())
+param1= "cpuLoad"
+param2= metric_df['value'].max()
+param3= metric_df['value'].min()
+param4= metric_df['value'].mean()
+param4= metric_df['value'].std()
+
+#########################################################################
+
+from confluent_kafka import Producer
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        sys.stderr.write('Usage: %s <bootstrap-brokers> <topic>\n' % sys.argv[0])
+        sys.exit(1)
+
+    broker = sys.argv[1]
+    topic = sys.argv[2]
+
+    # Producer configuration
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    conf = {'bootstrap.servers': broker}
+
+    # Create Producer instance
+    p = Producer(**conf)
+
+    # Optional per-message delivery callback (triggered by poll() or flush())
+    # when a message has been successfully delivered or permanently
+    # failed delivery (after retries).
+    def delivery_callback(err, msg):
+        if err:
+            sys.stderr.write('%% Message failed delivery: %s\n' % err)
+        else:
+            sys.stderr.write('%% Message delivered to %s [%d] @ %d\n' %
+                             (msg.topic(), msg.partition(), msg.offset()))
+
+    # Read lines from stdin, produce each line to Kafka
+    for line in sys.stdin:
+        try:
+            # Produce line (without newline)
+            p.produce(topic, line.rstrip(), callback=delivery_callback)
+
+        except BufferError:
+            sys.stderr.write('%% Local producer queue is full (%d messages awaiting delivery): try again\n' %
+                             len(p))
+
+        # Serve delivery callback queue.
+        # NOTE: Since produce() is an asynchronous API this poll() call
+        #       will most likely not serve the delivery callback for the
+        #       last produce()d message.
+        p.poll(0)
+
+    # Wait until all messages have been delivered
+    sys.stderr.write('%% Waiting for %d deliveries\n' % len(p))
+    p.flush()
+
+#########################################################################
+
+#for item1 in metric_object_list:
+#    with open('export_dataframe.csv', 'a') as f:
+#            csv_writer = csv.writer(f)
+#            csv_writer.writerow(fields)
+#            csv_writer.writerow(item1)
+#
+#print("#-------------------#")
 
 
 
