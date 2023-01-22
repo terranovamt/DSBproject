462156b955c97bcb5d1fb20a863e877a353525e1
Fabiola Marchì <mrcfbl99s48c342j@studium.unict.it>
2023-01-22 23:05:31 +0100

    stationarity & seasonability

--- ETL_datapipeline.py
+++ ETL_datapipeline.py
@@ -40,10 +40,11 @@ print('#------------Connection succed !------------#\n')
 #--------------------------------------------FIRST-----------------------------------------------------#
 #----------------------------------------PROMETHEUS' QUERY---------------------------------------------#
 time_to_evaluate = ['1h', '3h', '12h']
-metrics_to_evaluate = ['cpuLoad', 'availableMem', 'diskUsage']
+metrics_to_evaluate = ['cpuLoad', 'cpuTemp', 'diskUsage', 'availableMem', 'networkThroughput']
 end_time = parse_datetime("now")
 chunk_size = timedelta(minutes=10)
 label_config = {'job': 'summary'} 
+counter = 0
 for time in time_to_evaluate:       
     start_time = parse_datetime(time)
     for metric in metrics_to_evaluate:
@@ -55,45 +56,155 @@ for time in time_to_evaluate:
             chunk_size=chunk_size,
         )
         metric_df = MetricRangeDataFrame(metric_data) # Creazione del data frame
-        print('METRIC_DF -> ', metric_df)
+        #print('METRIC_DF -> ', metric_df)
         #metric_object_list = MetricsList(metric_data)
         minValues_list = []
         maxValues_list = []
         meanValues_list = []
         stdValues_list = []
+        Oneh_monitor = []
+        Twoh_monitor = []
+        Threeh_monitor = []
         #--------------------------Evaluation of MAX, min, mean and std_dev----------------------------#
+        print("Evaluation of MAX, min, mean and std_dev is on going \n")
+        print('                       ..........                    \n')
+        print('                       ..........                    \n')
         max_value = round(metric_df['value'].max(), 2)
         min_value = round(metric_df['value'].min(), 2)
         mean_value = round(metric_df['value'].mean(), 2)
         std_value = round(metric_df['value'].std(), 2)
-        print("\nMonitoring done!\n")
-        print("------>MAX value: ", max_value)
-        print("------>min value: ", min_value)
-        print("------>MEAN value: ", mean_value)
-        print("------>STD_dev value: ", std_value)
-        print('                     ..........              \n')
-        print('                     ..........              \n')
+        print("\n               Monitoring done!            \n")
+        print("---------------->MAX value: ", max_value)
+        print("---------------->min value: ", min_value)
+        print("---------------->MEAN value: ", mean_value)
+        print("---------------->STD_dev value: ", std_value)
+        print('                                           \n\n')
+        minValues_list.append(min_value) 
+        maxValues_list.append(max_value) 
+        meanValues_list.append(mean_value)
+        stdValues_list.append(std_value) 
         
         #-----------------Evaluation of AutoCorrelation, Stationarity & Seasonability-------------------#
-        print("Evaluation of AutoCorrelation on going ...\n")
+        print("Evaluation of AutoCorrelation is on going ...\n")
         autocorrelation = acf(metric_df['value']) 
         autocorrelation_list = autocorrelation.tolist()
         del autocorrelation_list[0]
         for autocorrelation_i in autocorrelation_list:
-            print('----> ', autocorrelation_i)
+            print('AUTOCORRELATION ----> ', autocorrelation_i)
 
-        print("Evaluation of Stationariety on going ...\n")
+        print("Evaluation of Stationariety is on going ...   \n")
         stationarity = adfuller(metric_df['value'],autolag='AIC') 
-        print('----> ', stationarity)
-        #stationarity_list = stationarity.tolist()
-        #for stationarity_i in stationarity_list:
-        #    print('----> ', stationarity_i)
+        print('STATIONARITY ----> ', stationarity)
 
-        #seasonability = seasonal_decompose(metric_df['value'], model='additive', period=10)
-        #seasonability_list = seasonability.seasonal.tolist() 
-        #for seasonability_i in seasonability_list:
-        #    print('----> ', seasonability_i)
+        print("Evaluation of Seasonability is on going ...   \n")
+        seasonability = seasonal_decompose(metric_df['value'], model='additive', period=10)
+        seasonability_list = seasonability.seasonal.tolist() 
+        for seasonability_i in seasonability_list:
+            print('SEASONABILITY ----> ', seasonability_i)
 
+        #-------------------------------Prediction of each metric--------------------------------------#
+        #---------->RESAMPLING
+        print("Prediction process is started now            \n")
+        print('                     ..........              \n')
+        print('                     ..........              \n')
+        print("RESAMPLING is on going ...                   \n")
+        MAX_resampling = metric_df['value'].resample(rule='2T').max()
+        print(" MAX RESAMPLING ---> \n", MAX_resampling)
+        min_resampling = metric_df['value'].resample(rule='2T').min()
+        print(" MIN RESAMPLING ---> \n", min_resampling)
+        mean_resampling = metric_df['value'].resample(rule='2T').mean()   
+        print(" MEAN RESAMPLING ---> \n", mean_resampling) 
+        
+        #---------->PREDICTION
+        print('                     ..........              \n')
+        print('                     ..........              \n')
+        print("PREDICTION is on going ...                   \n")
+        tsmodel_MAX = ExponentialSmoothing(MAX_resampling, trend='add', seasonal='add',seasonal_periods=5).fit()
+        tsmodel_min = ExponentialSmoothing(min_resampling, trend='add', seasonal='add',seasonal_periods=5).fit()
+        tsmodel_mean = ExponentialSmoothing(mean_resampling, trend='add', seasonal='add',seasonal_periods=5).fit() 
+        
+        prediction_max = tsmodel_MAX.forecast(5) 
+        print("Prediction of MAX ---> \n", prediction_max)
+        prediction_min = tsmodel_min.forecast(5)  
+        print("Prediction of min ---> \n", prediction_min)  
+        prediction_mean = tsmodel_mean.forecast(5)        
+        print("Prediction of mean ---> \n", prediction_mean) 
+
+        monitoring_values = {
+            "Metrics":{
+                "Metric Name": metric,
+                "Timing": time
+            } 
+        }
+        if (time == '1h'):
+            Oneh_monitor.append(monitoring_values)
+        if (time == '2h'):
+            Twoh_monitor.append(monitoring_values)
+        if (time == '3h'):
+            Threeh_monitor.append(monitoring_values)
+
+        counter += 1
+
+with open('output.txt', 'a') as f:
+    for item in minValues_list:
+        f.write('minValues_list')
+        f.write(item)
+        f.write('\n')
+        #print('minValues_list: \n', item)
+
+    for item in maxValues_list:
+        f.write('maxValues_list')
+        f.write(item)
+        f.write('\n')
+        #print('maxValues_list: \n', item)
+
+    for item in meanValues_list:
+        f.write('meanValues_list')
+        f.write(item)
+        f.write('\n')
+        #print('meanValues_list: \n', item)
+
+    for item in stdValues_list:
+        f.write('stdValues_list')
+        f.write(item)
+        f.write('\n')
+        #print('stdValues_list: \n', item)
+
+    for autocorrelation_i in autocorrelation_list:
+        f.write('stdValues_list')
+        f.write(autocorrelation_i)
+        f.write('\n')
+        #print('AUTOCORRELATION ----> ', autocorrelation_i)
+
+    f.write('STATIONARITY')
+    f.write(stationarity)
+    print('STATIONARITY ----> ', stationarity)
+
+    for seasonability_i in seasonability_list:
+        f.write('seasonability_i')
+        f.write(seasonability_i)
+        f.write('\n')
+        #print('SEASONABILITY ----> ', seasonability_i)
+
+    for item in Oneh_monitor:
+        f.write('Oneh_monitor')
+        f.write(item)
+        f.write('\n')
+        #print('1h Monitoring: \n', item)
+
+    for item in Twoh_monitor:
+        f.write('Twoh_monitor')
+        f.write(item)
+        f.write('\n')
+        #print('2h Monitoring: \n', item)
+
+    for item in Threeh_monitor:
+        f.write('Threeh_monitor')
+        f.write(item)
+        f.write('\n')
+        #print('3h Monitoring: \n', item) 
+
+print('Counter = ', counter)
 ########################################################################################################
 
 
